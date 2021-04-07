d150f1147ba0a689b8504cb4908c5b510d8a25da
Shaan Alam <shaanalam369@gmail.com>
2021-04-07 22:21:43 +0530

    added animations to the help section

--- src/components/HelpSection.js
+++ src/components/HelpSection.js
@@ -1,60 +1,75 @@
 import Volunteering from "../assets/images/volunteering.jpg";
 import Donation from "../assets/images/donation.jpg";
 import Awareness from "../assets/images/awareness.jpg";
+import Fade from "react-reveal/Fade";
 
 const HelpSection = () => {
   return (
     <div className="help__wrapper">
       <div className="container">
-        <h1>How can you help us?</h1>
+        <Fade top duration={2000}>
+          <h1>How can you help us?</h1>
+        </Fade>
         <div className="help__block">
           <div className="help__blockLeftContainer text">
-            <h1>Volunteering</h1>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
-              modi nobis itaque! Modi quasi accusamus iste magni commodi
-              voluptatibus aliquam accusantium totam eaque enim, doloribus
-              obcaecati. Exercitationem maxime consequatur at.
-            </p>
+            <Fade top duration={2000}>
+              <h1>Volunteering</h1>
+            </Fade>
+            <Fade top duration={2000} delay={200}>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+                modi nobis itaque! Modi quasi accusamus iste magni commodi
+                voluptatibus aliquam accusantium totam eaque enim, doloribus
+                obcaecati. Exercitationem maxime consequatur at.
+              </p>
+            </Fade>
           </div>
           <div className="help__blockRightContainer">
-            <img src={Volunteering} alt="Volunteering" />
+            <Fade left duration={2000} delay={400}>
+              <img src={Volunteering} alt="Volunteering" />
+            </Fade>
           </div>
         </div>
 
         <div className="help__block">
           <div className="help__blockLeftContainer">
-            <img src={Donation} alt="Donating books" />
+            <Fade right duration={2000}>
+              <img src={Donation} alt="Donating books" />
+            </Fade>
           </div>
           <div className="help__blockRightContainer text">
-            <h1>Donating books</h1>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
-              modi nobis itaque! Modi quasi accusamus iste magni commodi
-              voluptatibus aliquam accusantium totam eaque enim, doloribus
-              obcaecati. Exercitationem maxime consequatur at.
-            </p>
+            <Fade top duration={2000} delay={200}>
+              <h1>Donating books</h1>
+            </Fade>
+            <Fade top duration={2000} delay={400}>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+                modi nobis itaque! Modi quasi accusamus iste magni commodi
+                voluptatibus aliquam accusantium totam eaque enim, doloribus
+                obcaecati. Exercitationem maxime consequatur at.
+              </p>
+            </Fade>
           </div>
         </div>
 
         <div className="help__block">
           <div className="help__blockLeftContainer text">
-            <h1>Creating Awareness</h1>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
-              modi nobis itaque! Modi quasi accusamus iste magni commodi
-              voluptatibus aliquam accusantium totam eaque enim, doloribus
-              obcaecati. Exercitationem maxime consequatur at.
-            </p>
-            <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum
-              quibusdam non, aut ratione dolorum ea ipsum perspiciatis nobis
-              quidem quaerat eius totam obcaecati cum. Ipsa iure aperiam
-              explicabo nihil quae!
-            </p>
+            <Fade top duration={2000}>
+              <h1>Creating Awareness</h1>
+            </Fade>
+            <Fade top duration={2000} delay={200}>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+                modi nobis itaque! Modi quasi accusamus iste magni commodi
+                voluptatibus aliquam accusantium totam eaque enim, doloribus
+                obcaecati. Exercitationem maxime consequatur at.
+              </p>
+            </Fade>
           </div>
           <div className="help__blockRightContainer">
-            <img src={Awareness} alt="Creating awareness" />
+            <Fade left duration={2000} delay={400}>
+              <img src={Awareness} alt="Creating awareness" />
+            </Fade>
           </div>
         </div>
       </div>
