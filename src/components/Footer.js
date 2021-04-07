836363371d09ad465eb252f975fc28512ebb19fe
Shaan Alam <shaanalam369@gmail.com>
2021-04-07 22:33:36 +0530

    animated footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,30 +1,52 @@
+import Fade from "react-reveal/Fade";
+
 const Footer = () => {
   return (
     <footer>
       <div className="container">
-        <h1>Kids Empowerment Foundation</h1>
+        <Fade top duration={2000}>
+          <h1>Kids Empowerment Foundation</h1>
+        </Fade>
 
         <div className="footer__grid">
           <div className="footer__gridLeftContainer">
-            <h4>EMAIL - test1234@gmail.com</h4>
-            <h4>PHONE - 555 555 5555</h4>
-            <h4>Connect with us on</h4>
-            <a href="#!">
-              <i className="fa fa-facebook-official" aria-hidden="true"></i>
-            </a>
-            <a href="#!">
-              <i className="fa fa-instagram" aria-hidden="true"></i>
-            </a>
-            <a href="#!">
-              <i className="fa fa-youtube-play" aria-hidden="true"></i>
-            </a>
-            <a href="#!">
-              <i className="fa fa-linkedin-square" aria-hidden="true"></i>
-            </a>
+            <Fade top duration={2000} delay={200}>
+              <h4>EMAIL - test1234@gmail.com</h4>
+            </Fade>
+            <Fade top duration={2000} delay={400}>
+              <h4>PHONE - 555 555 5555</h4>
+            </Fade>
+            <Fade top duration={2000} delay={600}>
+              <h4>Connect with us on</h4>
+            </Fade>
+            <Fade top>
+              <a href="#!">
+                <i className="fa fa-facebook-official" aria-hidden="true"></i>
+              </a>
+            </Fade>
+            <Fade top>
+              <a href="#!">
+                <i className="fa fa-instagram" aria-hidden="true"></i>
+              </a>
+            </Fade>
+            <Fade top>
+              <a href="#!">
+                <i className="fa fa-youtube-play" aria-hidden="true"></i>
+              </a>
+            </Fade>
+            <Fade top>
+              <a href="#!">
+                <i className="fa fa-linkedin-square" aria-hidden="true"></i>
+              </a>
+            </Fade>
           </div>
           <div className="footer__gridRightContainer">
-            <h4>Address</h4>
-            <p>ABCD ROAD, PERSHING SQUARE, NEW YORK</p>
+            <Fade top duration={2000} delay={200}>
+              <h4>Address</h4>
+            </Fade>
+            <Fade top duration={2000} delay={400}>
+              <p>ABCD ROAD, PERSHING SQUARE, NEW YORK</p>
+            </Fade>
           </div>
         </div>
       </div>
