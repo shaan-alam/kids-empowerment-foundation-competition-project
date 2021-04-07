37c52f5fce943475e6288ad6c9efb5b6494630f2
Shaan Alam <shaanalam369@gmail.com>
2021-04-07 22:26:00 +0530

    animated contact section

--- src/components/ContactSection.js
+++ src/components/ContactSection.js
@@ -1,19 +1,31 @@
+import Fade from "react-reveal/Fade";
+
 const ContactSection = () => {
   return (
     <div className="contact__wrapper">
       <div className="container">
-        <h1>Contact Us</h1>
+        <Fade top>
+          <h1>Contact Us</h1>
+        </Fade>
         <form action="">
-          <div className="form__field">
-            <input type="text" placeholder="Name" />
-          </div>
-          <div className="form__field">
-            <input type="number" placeholder="Phone" />
-          </div>
-          <div className="form__field">
-            <textarea name="" id="" cols="30" rows="10"></textarea>
-          </div>
-          <button className="primary__btn">Send</button>
+          <Fade top duration={2000}>
+            <div className="form__field">
+              <input type="text" placeholder="Name" />
+            </div>
+          </Fade>
+          <Fade top duration={2000} delay={200}>
+            <div className="form__field">
+              <input type="number" placeholder="Phone" />
+            </div>
+          </Fade>
+          <Fade top duration={2000} delay={400}>
+            <div className="form__field">
+              <textarea name="" id="" cols="30" rows="10"></textarea>
+            </div>
+          </Fade>
+          <Fade top duration={2000} delay={500}>
+            <button className="primary__btn">Send</button>
+          </Fade>
         </form>
       </div>
     </div>
