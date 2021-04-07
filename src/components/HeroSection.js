e7ffb8f0a4466e000b081ea14715fce743520c5f
Shaan Alam <shaanalam369@gmail.com>
2021-04-07 21:48:38 +0530

    added animation in hero section

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,29 +1,38 @@
 import HeroImg from "../assets/images/hero__section.jpg";
+import Fade from "react-reveal/Fade";
 
 const HeroSection = () => {
   return (
     <div className="hero__wrapper">
       <div className="container">
         <div className="hero__wrapperLeft">
-          <h1>Kids Empowerment Foundation</h1>
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis,
-            libero laborum. Ipsum culpa voluptates cumque, quam corporis id
-            obcaecati expedita rem maiores, unde explicabo quaerat. Nisi
-            cupiditate ex quam quas?
-          </p>
-          <div className="hero__cta">
-            <a href="#!" className="primary__btn">
-              Get Started
-            </a>
-            <a href="#!" className="secondary__btn">
-              <i className="fa fa-play-circle" aria-hidden="true"></i> Play
-              Video
-            </a>
-          </div>
+          <Fade bottom duration={2000}>
+            <h1>Kids Empowerment Foundation</h1>
+          </Fade>
+          <Fade bottom duration={2000} delay={200}>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Corporis, libero laborum. Ipsum culpa voluptates cumque, quam
+              corporis id obcaecati expedita rem maiores, unde explicabo
+              quaerat. Nisi cupiditate ex quam quas?
+            </p>
+          </Fade>
+          <Fade bottom duration={2000} delay={400}>
+            <div className="hero__cta">
+              <a href="#!" className="primary__btn">
+                Get Started
+              </a>
+              <a href="#!" className="secondary__btn">
+                <i className="fa fa-play-circle" aria-hidden="true"></i> Play
+                Video
+              </a>
+            </div>
+          </Fade>
         </div>
         <div className="hero__wrapperRight">
-          <img src={HeroImg} alt="Boy in Yellow Dress" />
+          <Fade left duration={2000} delay={400}>
+            <img src={HeroImg} alt="Boy in Yellow Dress" />
+          </Fade>
         </div>
       </div>
     </div>
